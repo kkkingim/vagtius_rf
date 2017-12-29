59d5e535a1b8068d4906bb2714cf7ec0ecdfb473
vagitus <king@kk721.cn>
2017-12-29 16:14:16 +0900

    func1
    function list 1

---
+++ fun.py
@@ -0,0 +1,10 @@
+def tran(s):
+    if len(s) % 8 != 0:
+        return ''
+    if s.replace('1','') .replace('0','') != '':
+        return ''
+    sls = [s[i:i+8] for i in range(0, len(s) , 8)]
+    return ''.join([str(chr(int(i, 2))) for i in sls])
+
+
+print tran('011000110110011101101111')
