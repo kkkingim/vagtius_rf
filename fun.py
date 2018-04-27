633a91505183b613cee1b52cf9e615073fc32b26
Vagitus <kkkingim@vip.qq.com>
2018-04-27 22:36:20 +0900

    Update fun.py

--- fun.py
+++ fun.py
@@ -12,3 +12,25 @@ def bin2asc(s):
         return ''
     sls = [s[i:i+8] for i in range(0, len(s) , 8)]
     return ''.join([str(chr(int(i, 2))) for i in sls])
+
+
+#############################
+# translate a file to hex   #
+#############################
+def f2hex(f):
+    sh = ""
+    with open(f, "rb+") as f:
+        s = f.read()
+    i = 0
+    for c in s:
+        i+=1
+        ch = str(hex(ord(c))).replace("0x","")
+        if len(ch) == 1:
+            ch = "0" + ch
+        sh += ch + " "
+        if i % 4 == 0:
+            sh += " "
+        if i == 16:
+            i=0
+            sh += "\n"
+    return sh
